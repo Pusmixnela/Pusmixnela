1f742520ac5cd2dfa8438da46147600d035b9661
Pusmixnela <pusmixnella@gmail.com>
2024-10-25 07:14:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const books = [
-    { title: "Fakta Unik dan Super", category: "edukasi", link: "https://link-buku-fiksi-1.com" },
+   
     { title: "Biologi 1", category: "edukasi", link: "https://qrto.org/YRqDLM" },
     { title: "Why The Human Body", category: "edukasi", link: "https://books.google.co.id/books?id=9goREAAAQBAJ&lpg=PA2&hl=id&pg=PP1#v=onepage&q&f=false" },
    { title: "Teknik Kontrol Automatik", category: "edukasi", link: "https://www.google.co.id/books/edition/Teknik_Kontrol_Automatik/XNkDEAAAQBAJ?hl=id&gbpv=1&kptab=overview" },
-   { title: "Sosiologi Untuk Masyarakat Indonesia", category: "edukasi", link: "https://link-buku-fiksi-1.com" },
-   { title: "Inti Sari Fisika Untuk SMA", category: "edukasi", link: "https://link-buku-fiksi-1.com" },
-   { title: "Gen Z In Islam", category: "edukasi", link: "https://link-buku-fiksi-1.com" },
-];
+   
 
 function displayBooks(filteredBooks) {
     const bookList = document.getElementById('bookList');
